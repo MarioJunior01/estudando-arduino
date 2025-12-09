08610b0df2ebdea882140b16e1247a92c862e294
.l <136728961+MarioJunior01@users.noreply.github.com>
2025-12-09 08:41:17 -0400

    Update RoboOtto.ino

--- RoboOtto.ino
+++ RoboOtto.ino
@@ -1,90 +1,78 @@
+
+
 #include <Servo.h>
 
-Servo quadrilE;
-Servo peE;
-Servo quadrilD;
-Servo peD;
-#define TRIG 7
-#define ECHO 8
-#define BUZZER 6
+Servo pe1;
+Servo pe2;
+Servo perna1;
+Servo perna2;
 
+#define TRIG 8
+#define ECHO 9
+#define BUZZER 7
 
+int pos = 0;    
 
 void setup() {
-  peD.attach(9);
-  peE.attach(11);
-  quadrilE.attach(10);
-  quadrilD.attach(12);
-
-  pinMode(TRIG, OUTPUT);
+ pe1.attach(2);
+ pe2.attach(5);
+ perna1.attach(3);
+ perna2.attach(5);
+ pinMode(TRIG, OUTPUT);
   pinMode(ECHO, INPUT);
   pinMode(BUZZER, OUTPUT);
+ 
 }
 
 void loop() {
-
-
-  if (objetoFrente() < 10) {
-    tone(BUZZER, 400);
-    delay(3000);
-  } else {
-    andarFrente();
-    noTone(BUZZER);
-  }
-}
-
-void andarFrente() {
-
-  // 1. joga o peso para direita
-  peD.write(70);
-  peE.write(110);
-  delay(250);
-
-  // 2. quadril esquerdo vira para frente → passo
-  quadrilE.write(60);
-  delay(350);
-
-  // 3. volta pé ao normal
-  peD.write(90);
-  peE.write(90);
-  delay(200);
-
-
-  // ───────────────── PASSO DIREITO ─────────────────
-
-  // 1. joga o peso para esquerda
-  peD.write(110);
-  peE.write(70);
-  delay(250);
-
-  // 2. quadril direito vira para frente → passo
-  quadrilD.write(120);
-  delay(350);
-
-  // 3. volta pé ao normal
-  peD.write(90);
-  peE.write(90);
-  delay(200);
-}
-
-void dancar() {
-}
-
-long objetoFrente() {
   long duracao, distancia;
 
-  // Gera um pulso de 10us no TRIG
   digitalWrite(TRIG, LOW);
   delayMicroseconds(2);
   digitalWrite(TRIG, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIG, LOW);
+  duracao = pulseIn(ECHO, HIGH, 25000UL);
+  if (duracao == 0) {
+    distancia = -1;
+  } else {
+    distancia = duracao * 0.0343 / 2;
+  }
 
-  // Lê o tempo do pulso no ECHO
-  duracao = pulseIn(ECHO, HIGH);
+  Serial.print("Distancia: ");
+  Serial.print(distancia);
+  Serial.println(" cm");
+   if (distancia > 0 && distancia <= 5) {
+    tone(BUZZER, 400);
+    delay(3000);
+    for (pos = 0; pos <= 90; pos += 1) { 
+    pe1.write(pos);             
+    delay(15); 
+    
+   for (pos = 0; pos <= 90; pos += 1) { 
+    pe1.write(pos);             
+    delay(15);                       
+    }
+   } 
+  }
+  else {
+    noTone(BUZZER);
+  for (pos = 0; pos <= 90; pos += 1) { 
+    pe1.write(pos);             
+    delay(15);                       
+  }
+  for (pos = 90; pos >= 0; pos -= 1) { 
+    pe1.write(pos);              
+    delay(15);                      
+  }
+ for (pos = 180; pos >= 0; pos -= 1) { 
+    perna1.write(pos);              
+    delay(15);                      
+  }
+   for (pos = 180; pos >= 0; pos -= 1) { 
+    perna2.write(pos);              
+    delay(15);                      
+  }
 
-  // Converte o tempo em distância (cm)
-  distancia = duracao * 0.0343 / 2;
-
-  return distancia;
+  }
 }
